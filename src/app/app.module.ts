148b606684f3db3b9ece98a4e5b0b3e8b893f2d8
athulajayghosh <53214137+athulajayghosh@users.noreply.github.com>
2022-11-24 22:37:57 +0530

    Login and Reg

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminLoginComponent } from './admin-login/admin-login.component';
+import { RouterModule, Routes } from '@angular/router';
+import { UserLoginComponent } from './user-login/user-login.component';
+import { UserRegistrationComponent } from './user-registration/user-registration.component';
+
+const myRoute: Routes=[
+  {path:"",
+  component:AdminLoginComponent
+  },
+  {path:"userlogin",
+  component:UserLoginComponent
+  },
+  {path:"userregistration",
+  component:UserRegistrationComponent
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminLoginComponent,
+    UserLoginComponent,
+    UserRegistrationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(myRoute)
   ],
   providers: [],
   bootstrap: [AppComponent]
